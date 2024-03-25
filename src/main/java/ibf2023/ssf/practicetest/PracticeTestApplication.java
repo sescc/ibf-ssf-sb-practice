01df4f9f1eceaab79ac9ec5cc26aca0637527c42
sescc <codingscc@gmail.com>
2024-03-25 15:10:18 +0800

    Initial commit

---
+++ src/main/java/ibf2023/ssf/practicetest/PracticeTestApplication.java
@@ -0,0 +1,13 @@
+package ibf2023.ssf.practicetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibf2023/ssf/practicetest/PracticeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2023.ssf.practicetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
